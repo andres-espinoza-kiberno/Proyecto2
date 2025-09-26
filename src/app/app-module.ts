ae2c45cd5d4031e7abd61d9d37b27e2bcbd7c2d8
andres <andres.espinoza@kiberno.com>
2025-09-26 14:07:04 -0400

    Proyecto2

---
+++ src/app/app-module.ts
@@ -0,0 +1,24 @@
+import { NgModule, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing-module';
+import { App } from './app';
+import { Nav } from './nav/nav';
+import { ContainerComponent } from './Container/container.component';
+
+@NgModule({
+  declarations: [
+    App,
+    ContainerComponent,
+    Nav
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [
+    provideBrowserGlobalErrorListeners()
+  ],
+  bootstrap: [App]
+})
+export class AppModule { }
